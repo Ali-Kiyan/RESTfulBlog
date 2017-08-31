6e4d12518cc101b19083616dcaaea8b952a25249
Ali <alikiyand@gmail.com>
2017-09-01 00:03:24 +0100

    setting scaffoldings and displaying first blogg

---
+++ app.js
@@ -0,0 +1,61 @@
+var bodyParser   = require("body-parser"),
+mongoose         = require("mongoose")
+express          = require ("express"),
+app              = express();
+
+// app config
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+//mongoose model config
+var blogSchema = new mongoose.Schema({
+
+   title: String,
+   image: {type: String, default: "test.jpg"},
+   body: String,
+   created: {
+            type: Date,
+            default: Date.now
+            }
+
+
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+
+
+// RESTful ROUTES
+
+app.get("/", function(req, res){
+
+res.redirect("/blogs");
+
+});
+
+app.get("/blogs", function(req, res){
+
+ Blog.find({}, function(err, blogs){
+
+     if(err){
+        console.log(err);
+     }
+     else {
+       res.render("index", {blogs: blogs});
+     }
+
+ });
+
+
+
+});
+
+
+app.listen(3000,function(req, res){
+
+console.log("server is running");
+
+});
