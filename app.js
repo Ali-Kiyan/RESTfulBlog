cf2505f7d43683ff6335f96fd7d2915502feb28c
Ali <alikiyand@gmail.com>
2017-09-01 12:20:19 +0100

    functioning post to mongoDB

--- app.js
+++ app.js
@@ -36,6 +36,8 @@ res.redirect("/blogs");
 
 });
 
+//INDEX ROUTE
+
 app.get("/blogs", function(req, res){
 
  Blog.find({}, function(err, blogs){
@@ -50,6 +52,30 @@ app.get("/blogs", function(req, res){
  });
 
 
+
+});
+
+//NEW ROUTE
+app.get("/blogs/new",function(req, res){
+  res.render("new");
+});
+
+//CREATE ROUTE
+
+
+app.post("/blogs", function(req, res){
+
+Blog.create(req.body.blog, function(err, newBlog){
+
+if(err){
+  res.render("new");
+}
+else{
+
+res.redirect("/blogs");
+
+}
+});
 
 });
 
