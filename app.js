e1d3da5901ac11f8bf87d1a714aa9e1b51aef7a4
Ali <alikiyand@gmail.com>
2017-09-01 15:42:14 +0100

    full functoining crud

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var bodyParser   = require("body-parser"),
-mongoose         = require("mongoose")
+methodOverride   = require("method-override"),
+mongoose         = require("mongoose"),
 express          = require ("express"),
 app              = express();
+
 
 // app config
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+
 
 
 //mongoose model config
@@ -79,6 +84,65 @@ res.redirect("/blogs");
 
 });
 
+
+// SHOW ROUTE
+app.get("/blogs/:id",function(req, res){
+Blog.findById(req.params.id, function(err, foundBlog){
+
+if(err){
+  res.redirect("/blogs");
+}
+else{
+res.render("show", {blog: foundBlog});
+}
+
+});
+
+
+});
+//EDIT ROUTE
+
+app.get("/blogs/:id/edit",function(req, res){
+  Blog.findById(req.params.id,function(err, foundBlog){
+
+  if(err){
+    res.redirect("/blogs");
+  }
+  else{
+    res.render("edit",{blog: foundBlog});
+  }
+
+  });
+
+
+});
+// UPDATE ROUTE
+
+app.put("/blogs/:id", function(req, res){
+Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+    if(err){
+      res.redirect("/blogs");
+    }
+    else{
+      res.redirect("/blogs/" + req.params.id);
+    }
+ });
+});
+
+// DELETE ROUTE
+
+app.delete("/blogs/:id", function(req, res){
+Blog.findByIdAndRemove(req.params.id, function(err){
+if(err){
+  res.redirect("/blogs");
+}
+else{
+  res.redirect("/blogs");
+  //for now
+}
+});
+
+});
 
 app.listen(3000,function(req, res){
 
